6601765770247be3e5b372c19c07dacd4f8a559a
Leslie Atterbury <atterbur@unlv.nevada.edu>
2024-09-20 21:20:02 -0700

    Added DELETE method
    Counter.py now can respond correctly to a DELETE request.

--- tdd/src/counter.py
+++ tdd/src/counter.py
@@ -50,3 +50,19 @@ def get_counter(name):
         return {"Message": f"Counter {name} was not found"}, status.HTTP_404_NOT_FOUND
 
     return str(COUNTERS[name]), status.HTTP_200_OK
+
+
+@app.route("/counters/<name>", methods=["DELETE"])
+def delete_counter(name):
+    """Deletes a counter"""
+
+    app.logger.info(f"Delete the counter: {name}")
+
+    global COUNTERS
+
+    if name not in COUNTERS:
+        return {"Message": f"Counter {name} was not found"}, status.HTTP_404_NOT_FOUND
+
+    del COUNTERS[name]
+
+    return {}, status.HTTP_204_NO_CONTENT
